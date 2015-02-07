e3c1e8560ef525cb318d7482fbef3f824acb8d88
TCYRUS <tcyrus@users.noreply.github.com>
2015-02-07 16:19:22 -0500

    Retrying Test Case

--- php/getfeed.php
+++ php/getfeed.php
@@ -8,23 +8,24 @@ $friends=$_GET["friends"];
  * Store reg id in users table
  */
 
-if (isset($friends))
+echo '{"challenges":[{"friend":"782093555198877","desc":"This is an example challenge used for testing purposes."}]}';
+
+/*if (isset($friends))
 {
     $friends=json_decode($friends);
     // Store user details in db
     $res = getChallengeList($friends);
-    echo '{"challenges":[{"friend":"782093555198877","desc":"This is an example challenge used for testing purposes."}]}';
-/*    if(res)
+    if(res)
     {
     	echo json_encode($res);
     }
     else
     {
        http_response_code(404);
-*/    }
+    }
 } 
 else 
 {
     // user details not found
-}
+}*/
 ?>
