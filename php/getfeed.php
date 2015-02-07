5b593f28693dff88b5a562b54217871970821fb5
TCYRUS <tcyrus@users.noreply.github.com>
2015-02-07 16:51:25 -0500

    Updates to PHP

--- php/getfeed.php
+++ php/getfeed.php
@@ -1,26 +1,31 @@
 <?php
+echo 1
 require_once('loader.php');
-
-$friends = $_POST["friends"];
-
+echo 2
+$friends=$_POST["friends"];
+echo 3
 /**
  * Registering a user device in database
  * Store reg id in users table
  */
 
 //echo '{"challenges":[{"friend":"782093555198877","desc":"This is an example challenge used for testing purposes."}]}';
-
+echo 4
 if (isset($friends)) {
+  echo 5
     $friends=json_decode($friends)[0];
     print_r($friends);
     // Store user details in db
+    echo 8
     $res     = getChallengeList($friends);
+    echo 7
     if (res) {
         echo json_encode($res);
     } else {
         http_response_code(404);
     }
 } else {
+  echo 6
     // user details not found
 }
 ?>
