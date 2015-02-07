74971d4c9ae63dec552f8f96c275c3500efc250c
TCYRUS <tcyrus@users.noreply.github.com>
2015-02-07 16:13:50 -0500

    Adding Static Data

--- php/getfeed.php
+++ php/getfeed.php
@@ -7,20 +7,21 @@ $friends=$_GET["friends"];
  * Registering a user device in database
  * Store reg id in users table
  */
+
 if (isset($friends))
 {
     $friends=json_decode($friends);
     // Store user details in db
     $res = getChallengeList($friends);
- 
-    if(res)
+    echo '{"challenges":[{"friend":"782093555198877","desc":"This is an example challenge used for testing purposes."}]}';
+/*    if(res)
     {
     	echo json_encode($res);
     }
     else
     {
        http_response_code(404);
-    }
+*/    }
 } 
 else 
 {
