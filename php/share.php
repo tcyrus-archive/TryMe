55fa2f650a7c795f75011db7d14e6ab9159aa687
TCYRUS <tcyrus@users.noreply.github.com>
2015-02-08 02:14:38 -0500

    Completed Share

--- php/share.php
+++ php/share.php
@@ -1,4 +1,19 @@
 <?php
 
+require_once('loader.php');
+require_once('push.php');
 
+$fb_id=$_GET["fb_id"];
+$message=$_GET["message"];
+
+/**
+ * Registering a user device in database
+ * Store reg id in users table
+ */
+
+if (isset($fb_id)) {
+    // Store user details in db
+    $gcm_id=getGCM($fb_id);
+    send_push_notification([$gcm_id],$message);
+}
 ?>
